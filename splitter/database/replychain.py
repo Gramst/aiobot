55ee544c7fe6616f488efd232d40a0ad6dc5511b
Artem <Gramst@users.noreply.github.com>
2020-07-23 16:17:56 +0300

    fix

--- splitter/database/replychain.py
+++ splitter/database/replychain.py
@@ -1,14 +1,22 @@
 import sqlite3
 import aiosqlite
+import os.path
 
 class ReplyChain:
 
     def __init__(self, path, base_name):
         self.path = path
         self.base_name = base_name
-        conn = sqlite3.connect(self.path + base_name)
+        if not os.path.isfile(self.path + self.base_name):
+            with open(self.path + self.base_name, 'w') as f:
+                pass
+        self.make_db_table()
+        
+
+    def make_db_table(self):
+        conn = sqlite3.connect(self.path + self.base_name)
         c = conn.cursor()
         c.execute('''CREATE TABLE stocks
-             (fromTID INTEGER, fromMID INTEGER, toTID INTEGER, toMID INTEGER, time INTEGER)''')
+            (fromTID INTEGER, fromMID INTEGER, toTID INTEGER, toMID INTEGER, time INTEGER)''')
         conn.commit()
         conn.close()
