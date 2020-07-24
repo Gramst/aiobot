8b63882fa5586b2d1106d12fa174dc09db38e171
Artem Aleksandrovich <jaartbar@gmail.com>
2020-07-24 11:47:05 +0300

    fix? add jobs

--- splitter/splitter_cls.py
+++ splitter/splitter_cls.py
@@ -3,19 +3,28 @@ from .database import ReplyChain
 
 from aiohttp import web
 import asyncio 
+from datetime import datetime
 
 class Splitter:
+    jobs: list
 
     def __init__(self, token: str, bases_path: str):
         self.token = token
         self.base_url: str = f'https://api.telegram.org/bot{self.token}/'
         self.in_queue = asyncio.Queue()
         self.reply_chain = ReplyChain(bases_path, 'reply.db')
+        self.jobs = []
 
     async def income_msg(self, request) -> InMessage:
         data = await request.json()
         self.in_queue.put_nowait(InMessage(data))
         return web.Response(status=200)
+
+    async def kronos(self):
+        [i.update_timer() for i in self.jobs]
+        _ = [i for i in jobs if i.ready]
+        for job in _:
+            await job.run()
 
     async def process(self):
         while True:
