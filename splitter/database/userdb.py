c82308ef54176130e949b11db0f4e21acafdbccb
Artem <Gramst@users.noreply.github.com>
2020-07-29 11:04:54 +0300

    Мучаем юзера

--- splitter/database/userdb.py
+++ splitter/database/userdb.py
@@ -8,6 +8,7 @@ from typing import List
 
 
 class User:
+    VERSION : int = 1
     chat_id : int
     banned  : int
     active  : int
@@ -22,6 +23,15 @@ class User:
 
     def __repr__(self):
         return f'User: chat_id {self.chat_id}, banned={self.banned}, active={self.active}, f_new={self.f_new};'
+
+    @classmethod
+    def check_version(cls, other: User) -> User:
+        if cls.VERSION == getattr(other, 'VERSION', 0):
+            return other
+        res = cls(other.chat_id)
+        res.banned = getattr(other, 'banned', 0)
+        res.active = getattr(other, 'active', 1)
+        return res
 
 
 class UsersDB:
@@ -38,7 +48,7 @@ class UsersDB:
             with open(self.path + self.base_name, 'w+'):
                 pass
         self.make_db_table()
-        
+
 
     def make_db_table(self):
         conn = sqlite3.connect(self.path + self.base_name, detect_types=sqlite3.PARSE_DECLTYPES)
@@ -47,26 +57,34 @@ class UsersDB:
         conn.commit()
         conn.close()
 
+
     async def add_data(self, user: User):
+        sql = f"INSERT INTO {self.table_name} VALUES (?,?,?,?)"
         async with aiosqlite.connect(self.path + self.base_name, detect_types=sqlite3.PARSE_DECLTYPES) as db:
-            await db.execute(f"INSERT INTO {self.table_name} VALUES (?,?,?,?)", (user.chat_id, user, user.banned, user.active))
+            await db.execute(sql, [(user.chat_id), (user), (user.banned), (user.active)])
+            await db.commit()
+
+    async def update_data(self, user: User):
+        sql = f"UPDATE {self.table_name} SET user_data=?, ban=?, active=? WHERE id=?"
+        async with aiosqlite.connect(self.path + self.base_name, detect_types=sqlite3.PARSE_DECLTYPES) as db:
+            await db.execute(sql, (user, user.banned, user.active, user.chat_id))
             await db.commit()
 
 
     async def get_data(self, chat_id: int) -> User:
+        sql = f"SELECT * FROM {self.table_name} WHERE id=?"
         async with aiosqlite.connect(self.path + self.base_name, detect_types=sqlite3.PARSE_DECLTYPES) as db:
-            sql = f"SELECT * FROM {self.table_name} WHERE id=?"
             async with db.execute(sql, [(chat_id)]) as cursor:
                 res = await cursor.fetchone()
             if res:
-                return res[1]
+                return User.check_version(res[1])
             else:
                 return User(chat_id)
 
     async def get_active(self) -> List[User]:
+        sql = f"SELECT user_data FROM {self.table_name} WHERE active BETWEEN 1 and 1"
         res = []
         async with aiosqlite.connect(self.path + self.base_name, detect_types=sqlite3.PARSE_DECLTYPES) as db:
-            sql = f"SELECT user_data FROM {self.table_name} WHERE active BETWEEN 1 and 1"
             async with db.execute(sql) as cursor:
                 res = await cursor.fetchmany()
         return res
