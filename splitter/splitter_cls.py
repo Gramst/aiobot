a71355f0b58b988b66adc91045d2f91e1fea6ff5
Artem <Gramst@users.noreply.github.com>
2020-08-17 11:32:50 +0300

    test systemd unit

--- splitter/splitter_cls.py
+++ splitter/splitter_cls.py
@@ -66,7 +66,6 @@ class Splitter:
                 out.promt = master.nick + ' : '
                 out << income
                 await out.get_reply_block()
-                #self.out_queue.put_nowait(out)
                 out.set_destination([i.chat_id for i in self.users_list]) # if i.chat_id != master.chat_id]
                 self.out_queue.put_nowait(out)
                 await self.user_database.update_data(master)
